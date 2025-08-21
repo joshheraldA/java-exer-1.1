af4321883c6c2213810ac5104a4380d4600458a7
Josh Herald Abelardo <joshheraldabelardo@Ikyangs-MacBook-Pro.local>
2025-08-21 15:16:09 +0800

    Added and tested checking account

--- src/Account.java
+++ src/Account.java
@@ -1,47 +1,77 @@
 abstract class Account {
     private final String accountNum;
     float balance;
-    public final float BALANCE_DEFAULT  = 100.0F;
+    public final float BALANCE_DEFAULT  = 100.0F, MINIMAL_AMOUNT;
+
     static Packages[] packages = new Packages[]{
             new Random_Package(),
             new Scanner_Package()
     };
 
-    public Account() {
+    public Account(float minimal) {
         accountNum =  packages[1].get_result();
         this.balance = BALANCE_DEFAULT;
+        this.MINIMAL_AMOUNT = minimal;
     }
 
-    abstract void withdraw(float amount);
+    abstract void deposit(float amount);
 
-    public void deposit(float amount) { set_balance(amount + get_balance()); }
+    public void withdraw(float amount) {
+        if(get_balance() - amount < getMINIMAL_AMOUNT()) {
+            System.err.println("[ERROR] Your balance does not have enough");
+            return;
+        }
+
+        set_balance(get_balance() - amount);
+        System.out.println("[MESSAGE] Your account balance is " + get_balance());
+    }
+
     public float get_balance() { return this.balance; }
     public void set_balance(float amount) { this.balance = amount; }
     public String get_account_num() { return this.accountNum; }
+    public float getMINIMAL_AMOUNT() { return this.MINIMAL_AMOUNT; }
 }
 
 
 class SavingAccount extends Account {
-    public final float MINIMAL_AMOUNT = 300.0F;
     public SavingAccount() {
-        super();
+        super(300.0F);
     }
 
     @Override
-    public void withdraw(float amount) {
-        if(get_balance() - amount < MINIMAL_AMOUNT) {
-            System.err.println("[ERROR] Not enough money");
+    public void deposit(float amount) {
+        if(amount <= 0) {
+            System.err.println("[ERROR] Amount should be positive");
             return;
         }
-        System.out.println("HELLO WORLD");
-        System.out.println("This is josh branch");
-        set_balance(get_balance() - amount); }
+
+        set_balance(amount + get_balance());
+        System.out.println("[MESSAGE] Your account balance is " + get_balance());
+    }
+}
+
+
+class CheckingAccount extends Account {
+    private final float BALANCE_LIMIT;
+
+    public CheckingAccount(float limit) {
+        super(0);
+        BALANCE_LIMIT = limit;
+    }
+
+    @Override
+    public void deposit(float amount) {
+        if(amount + get_balance() > BALANCE_LIMIT) {
+            System.err.println("[ERROR] Your balance with reach the balance limit");
+            return;
+        }
+
+        set_balance(amount + get_balance());
+        System.out.println("[MESSAGE] Your account balance is " + get_balance());
+    }
 }
 
 /*
-public class CheckingAccount extends Account {
-w
-}
 
 public class LaunderingAccount extends Account {
 float dirty_cash;
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        SavingAccount savingAccount = new SavingAccount();
+        savingAccount.deposit(1000);
+        savingAccount.withdraw(300);
+        System.out.println(savingAccount.get_balance());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        CheckingAccount checkingAccount = new CheckingAccount(1000);
+        checkingAccount.deposit(300);
+        System.out.println(checkingAccount.get_balance());
     }
 }
